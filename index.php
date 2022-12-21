9d34c34aaf2e7dc66b7df40452c55caead1d1acf
David Kirby <david@kinetic.com>
2022-12-21 15:42:02 -0600

    added files

---
+++ index.php
@@ -0,0 +1,28 @@
+<nav>
+    <p>mass effect fansite</p>
+
+    <ul>
+        <li><a href="http://">link</a></li>
+        <li><a href="http://">link</a></li>
+        <li><a href="http://">link</a></li>
+    </ul>
+    
+</nav>
+
+<?php include "header.php"; ?>
+
+<header>
+    <img src="resources/images/N7logo.jpg" alt="logo">
+    <h1>welcome</h1>
+</header>
+
+<div class="content">
+
+<main>
+
+<h2>main</h2>
+
+</main>
+</div>
+
+<?php include "footer.php"; ?>
